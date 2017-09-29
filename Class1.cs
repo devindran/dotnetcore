d786461459a4c10af14cb6b3c2f4c955170a15e5
Devindran Ramadass <Devindran_Ramadass@Dell.com>
2017-09-29 10:37:39 +0800

    initial checkin

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnetcore
+{
+    public class Class1
+    {
+        public string Test()
+        {
+            return "hello world";
+        }
+    }
+}
